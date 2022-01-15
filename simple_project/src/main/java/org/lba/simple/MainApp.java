48b4cc598cca9a475aceebb61dc7355501743bbe
d3vsupp0r7 <d3vsupp0r7@example.com>
2022-01-15 12:16:05 +0100

    BAseline

---
+++ simple_project/src/main/java/org/lba/simple/MainApp.java
@@ -0,0 +1,10 @@
+package org.lba.simple;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+
+	}
+
+}
+
---
+++ simple_project/src/main/java/org/lba/simple/SimpleObject.java
@@ -0,0 +1,15 @@
+package org.lba.simple;
+
+import java.io.Serializable;
+
+public class SimpleObject implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8787892550638439369L;
+	
+	
+	
+}
+
