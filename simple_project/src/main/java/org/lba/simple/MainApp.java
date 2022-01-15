fdb4c4b09d14573d604704fbc714e00fe9516943
d3vsupp0r7 <d3vsupp0r7@example.com>
2022-01-15 12:22:39 +0100

    Commit for conflcit

--- simple_project/src/main/java/org/lba/simple/MainApp.java
+++ simple_project/src/main/java/org/lba/simple/MainApp.java
@@ -4,7 +4,30 @@ public class MainApp {
 
 	public static void main(String[] args) {
 
+		// TODO Auto-generated method stub
+		SimpleObject obj = new SimpleObject();
+		obj.setMessage("Hello Conflict example");
+		/**/
+		System.out.println("Out: " + obj.toString());
 	}
 
+	private String message;
+
+	public SimpleObject() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "SimpleObject [message=" + message + "]";
+	}
 }
 
