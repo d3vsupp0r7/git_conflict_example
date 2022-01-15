dac654f2b3ed29989ba921d14eba3c8a77415f33
d3vsupp0r7 <36088952+d3vsupp0r7@users.noreply.github.com>
2022-01-15 12:18:23 +0100

    Update SimpleObject.java

--- simple_project/src/main/java/org/lba/simple/SimpleObject.java
+++ simple_project/src/main/java/org/lba/simple/SimpleObject.java
@@ -9,6 +9,24 @@ public class SimpleObject implements Serializable {
 	 */
 	private static final long serialVersionUID = -8787892550638439369L;
 	
+	private String message;
+	
+	public SimpleObject() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "SimpleObject [message=" + message + "]";
+	}
 	
 	
 }
