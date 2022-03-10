8fb8af78c415b7046587385f03263d81f001ba1e
d3vsupp0r7 <d3vsupp0r7@example.com>
2022-03-10 07:21:07 +0100

    Added featureGitWork - 1

---
+++ simple_project/src/main/java/org/lba/simple/git/gitwork1/FeatureGitWork1.java
@@ -0,0 +1,33 @@
+package org.lba.simple.git.gitwork1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FeatureGitWork1 {
+
+	public List<String> getFeatureGitWork1Values(){
+		
+		List<String> values = new ArrayList<>();
+		/**/
+		values.add("commanValue1_FeatureGitWork1");
+		values.add("commanValue2_FeatureGitWork1");
+		values.add("commanValue3_FeatureGitWork1");
+		values.add("commanValue4_FeatureGitWork1");
+		values.add("commanValue5_FeatureGitWork1");
+		values.add("commanValue6_FeatureGitWork1");
+		values.add("commanValue7_FeatureGitWork1");
+		values.add("commanValue8_FeatureGitWork1");
+		values.add("commanValue9_FeatureGitWork1");
+		values.add("commanValue10_FeatureGitWork1");
+		/**/
+		return values;
+	}
+	
+	private void methodCommonFeatureGitWork1_1() {
+		
+	}
+	
+	private void methodCommonFeatureGitWork1_2() {
+		
+	}
+}
