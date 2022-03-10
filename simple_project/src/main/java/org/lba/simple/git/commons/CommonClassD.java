2940ab8b6f601dece876fa0f03011d201d4417a3
d3vsupp0r7 <d3vsupp0r7@example.com>
2022-03-10 07:22:27 +0100

    Commint common class C,D

---
+++ simple_project/src/main/java/org/lba/simple/git/commons/CommonClassD.java
@@ -0,0 +1,33 @@
+package org.lba.simple.git.commons;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommonClassD {
+
+	public List<String> getValues(){
+		
+		List<String> values = new ArrayList<>();
+		/**/
+		values.add("commanValue1_ClassD");
+		values.add("commanValue2_ClassD");
+		values.add("commanValue3_ClassD");
+		values.add("commanValue4_ClassD");
+		values.add("commanValue5_ClassD");
+		values.add("commanValue6_ClassD");
+		values.add("commanValue7_ClassD");
+		values.add("commanValue8_ClassD");
+		values.add("commanValue9_ClassD");
+		values.add("commanValue10_ClassD");
+		/**/
+		return values;
+	}
+	
+	private void methodCommonClassD_1() {
+		
+	}
+	
+	private void methodCommonClassD_2() {
+		
+	}
+}
