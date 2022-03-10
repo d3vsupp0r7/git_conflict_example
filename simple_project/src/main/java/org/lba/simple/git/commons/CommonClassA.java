becde0ecbd393f16d2de75bb9808babf47685a7f
d3vsupp0r7 <d3vsupp0r7@example.com>
2022-03-10 07:21:58 +0100

    Commit Common Class A,B

---
+++ simple_project/src/main/java/org/lba/simple/git/commons/CommonClassA.java
@@ -0,0 +1,25 @@
+package org.lba.simple.git.commons;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommonClassA {
+
+	public List<String> getValues(){
+		
+		List<String> values = new ArrayList<>();
+		/**/
+		values.add("commanValue1");
+		values.add("commanValue2");
+		values.add("commanValue3");
+		values.add("commanValue4");
+		values.add("commanValue5");
+		values.add("commanValue6");
+		values.add("commanValue7");
+		values.add("commanValue8");
+		values.add("commanValue9");
+		values.add("commanValue10");
+		/**/
+		return values;
+	}
+}
---
+++ simple_project/src/main/java/org/lba/simple/git/commons/CommonClassB.java
@@ -0,0 +1,33 @@
+package org.lba.simple.git.commons;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommonClassB {
+
+	public List<String> getValues(){
+		
+		List<String> values = new ArrayList<>();
+		/**/
+		values.add("commanValue1_ClassB");
+		values.add("commanValue2_ClassB");
+		values.add("commanValue3_ClassB");
+		values.add("commanValue4_ClassB");
+		values.add("commanValue5_ClassB");
+		values.add("commanValue6_ClassB");
+		values.add("commanValue7_ClassB");
+		values.add("commanValue8_ClassB");
+		values.add("commanValue9_ClassB");
+		values.add("commanValue10_ClassB");
+		/**/
+		return values;
+	}
+	
+	private void methodCommonClassB_1() {
+		
+	}
+	
+	private void methodCommonClassB_2() {
+		
+	}
+}
